cee9b0c60d43a69b7331576c3d55e9b905a096cc
DeathKing <deathking0622@gmail.com>
2013-02-11 16:56:08 +0800

    修改了separete.rb，去掉了调试信息。

--- Srt/separate.rb
+++ Srt/separate.rb
@@ -1,40 +1,40 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 if ARGV.empty?
-    puts "Separate - Seprate Duo-Language into Two Single File"
-    puts "Usage: separate srt_file"
-    puts "Writen by DeathKing"
+    puts "Separate - 分割单个双语字幕为两个字幕。"
+    puts "用法: separate srt_file"
+    puts "说明：如果出错了，尝试调整一下newline。"
+    puts "\nWriten by DeathKing"
     exit
-end
-
-newline = case RUBY_PLATFORM
-when /linux|bsd|solaris/
-    "\n"
-when /darwin|mac os/
-    "\r"
-when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-    "\r\n"
 end
 
 eng = []
 chn = []
 cont = []
 
+# 换行符，由于文件比较混乱，有时需要自行调整
+# 如有问题，可以尝试调整为 \n、\r、\r\n中的其它值
+newline = "\n"
+
 open(ARGV[0]).each_line { |line|
-    puts line.inspect
     if line == newline
-        # a block is full
+        # 一个块 
         eng << cont[0] << cont[1]
         chn << cont[0] << cont[1]
+        # 非字幕块，即为制作信息时的处理
+        # 把内容全拷贝到中文字幕文件中
+        # 英文字幕用newline占位
         if cont.size != 4
             for i in 2...cont.size
                 chn << cont[i]
             end
-            chn << "newline"
-            eng << "-- Remain to be recheck  -- #{newline}"
+            chn << newline
+            eng << newline << newline
         else
-            chn << cont[2] << newline;
-            eng << cont[3] << newline;
+            # 拷贝到对应文件中去
+            chn << cont[2] << newline
+            eng << cont[3] << newline
         end
         cont.clear
     else
