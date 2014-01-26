aea163314eaabe648e31d407b85ef1bfd1ac8f91
DeathKing <DeathKing0622@gmail.com>
2014-01-26 10:15:53 +0800

    add a video download script.

---
+++ Tools/download-sicp-movies.sh
@@ -0,0 +1,22 @@
+# Script by bjartwoIf
+# From https://gist.github.com/bjartwolf/3471090
+wget http://ia700402.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec1a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec1b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec2a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec2b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec3a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec3b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec4a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec4b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec5a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec5b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec6a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec6b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec7a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec7b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec8a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec8b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec9a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec9b.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec10a.mp4
+wget http://ia700401.us.archive.org/8/items/MIT_Structure_of_Computer_Programs_1986/lec10b.mp4
