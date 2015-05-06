7c0f5a478b94db7a6eacc7b5032dd14ae30e531a
DeathKing <deathking0622@gmail.com>
2015-05-06 12:34:00 +0800

    adjust the table generator.

--- Tools/lec2tabel.rb
+++ Tools/lec2tabel.rb
@@ -10,7 +10,13 @@ AUTHORS = {
   "Michael Savior" => "https://github.com/mut0u"
 }
 
-FORMAT = "| %-s | %-s | %-s | %-s | %-s | %-s |" 
+FORMAT = "| %-s | %-s | %-s | %-s | %-s | %-s |"
+
+ICON = {
+  "youtube" => "https://cloud.githubusercontent.com/assets/895809/7487454/7bcde098-f3ea-11e4-85be-d267459c4974.png",
+  "youku"   => "https://cloud.githubusercontent.com/assets/895809/7487453/7ad84a02-f3ea-11e4-9af4-2f8c4dc8679d.png",
+  "baidu"   => "https://cloud.githubusercontent.com/assets/895809/7487452/7aca0af0-f3ea-11e4-97a2-9ad4af3c6e2e.png"
+}
 
 def render_authors(authors)
   res = []
@@ -24,13 +30,21 @@ def render_as_mdlink(text, link)
   "[#{text}](#{link})"
 end
 
+def render_as_mdpic(alt, link)
+  "![#{alt}](#{link})"
+end
+
+def render_as_mdpiclink(picurl, text, link)
+  render_as_mdlink render_as_mdpic(text, picurl), link
+end
+
 def itemize(row)
   FORMAT % [
     row["id"],
     "《" + row["title"] + "》",
-    render_as_mdlink("优酷", row["youku"]),
-    render_as_mdlink("YouTube", row["youtube"]),
-    render_as_mdlink("百度网盘", row["baidu"]),
+    render_as_mdpiclink(ICON["youku"],      "优酷", row["youku"]),
+    render_as_mdpiclink(ICON["youtube"], "YouTube", row["youtube"]),
+    render_as_mdpiclink(ICON["baidu"],   "百度网盘", row["baidu"]),
     render_authors(row["translator"])
   ]
 end
@@ -47,7 +61,7 @@ def table_head
 end
 
 def divider
-  "| ---- | ---- | ---- | ------- | -------- | ---- |"
+  "| ---- | ---- |:----:|:-------:|:--------:| ---- |"
 end
 
 content = File.open('lec.json').read
