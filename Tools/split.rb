7ab4d016862bb499c04b9e78f051d1c74c45441d
DeathKing <deathking0622@gmail.com>
2015-08-10 23:47:21 +0800

    add some useful script.

--- Tools/split.rb
+++ Tools/split.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
+require_relative 'util'
+
+def help
+  doc = <<-HELP
+
+  HELP
+end
+
+
+filename = ARGV.first
+
+backup_file(filename)
+
+puts File.open(filename).each_line.map do |line|
+  line.gsub! "\n", ""
+  line.gsub! ". ", ".\n"
+  line.strip!
+  line
+end.join
